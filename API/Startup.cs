350cd501370e73c573f7da2924ec5c20087c4ae0
Ali Arda İsenkul <aarda.isenkul@tedu.edu.tr>
2020-10-27 15:56:26 +0300

    Güncel

--- API/Startup.cs
+++ API/Startup.cs
@@ -31,8 +31,12 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-            services.AddControllers();
-            
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy",policy =>{
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,10 +53,14 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            //app.UseMvc();
+            
         }
     }
 }
